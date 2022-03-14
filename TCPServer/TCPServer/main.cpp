1b6c8e1969ae45098cac8be56537ad8b5122d30d
Logan Nguyen <Lgad31vn@gmail.com>
2022-03-14 12:13:22 -0400

    init tcp winsock

---
+++ TCPServer/TCPServer/main.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+
+# include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+
+void main()
+{
+	// Initialize winsock
+
+	// Create a socket
+
+	// Bind the socket to an ip address and port
+
+	// Tell winsock the socket is for listening
+
+	// Wait for connection
+
+	// Close listening socket
+
+	// while loop: accept and echo message back to client
+
+
+
+	// Close the sock
+
+	// Shutdown winsock
+	
+}
